1380d00a6895a9a4656bcbc4a089cbbf8b5d2391
ArielFeingold <feingold.ariel@gmail.com>
2018-11-05 15:06:32 -0500

    added rails api

--- client/src/views/WarmupPage/WarmupPage.jsx
+++ client/src/views/WarmupPage/WarmupPage.jsx
@@ -70,7 +70,7 @@ class WarmupPage extends React.Component {
                 </GridItem>
               </GridContainer>
               <GridContainer justify="center">
-                <GridItem xs={12} sm={12} md={10} className={classes.navWrapper}>
+                <GridItem xs={12} sm={12} md={12} className={classes.navWrapper}>
                   <NavPills
                     alignCenter
                     color="primary"
